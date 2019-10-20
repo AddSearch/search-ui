81b15ffba958309a05b7c7f5fad09e2c19fa8315
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-20 17:38:14 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,33 +1,77 @@
-'use strict';
+import SearchBar from './searchbar/searchbar';
+import searchresults from './searchresults';
 
-var searchbar = require('./searchbar');
-var searchresults = require('./searchresults');
+import reducers from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+const store = createStore(reducers, applyMiddleware(thunk));
 
+//store.subscribe(() => console.log(store.getState()));
 
-var searchui = function(client) {
+export function getStore() {
+  return store;
+}
 
-  this.client = client;
-  this.searchResultsConf = null;
+function observeStore(store, select, onChange, key) {
+  let currentState = {};
 
-
-  /**
-   * Add a search bar
-   */
-  this.searchBar = function(conf) {
-    var self = this;
-    var cb = function(results) {
-      self.resultsCallback(results, self)
+  function handleChange() {
+    let nextState = select(store.getState());
+    if (nextState !== currentState[key]) {
+      console.log('State changed for ' + key);
+      currentState[key] = nextState;
+      onChange(currentState);
     }
+    else {
+      console.log('State unchanged for ' + key);
+    }
+  }
 
-    searchbar(this.client, cb, conf);
+  let unsubscribe = store.subscribe(handleChange);
+  handleChange();
+  return unsubscribe;
+}
+observeStore(getStore(), state => { return state.keyup.keyword; }, s => console.log('Keyword changed: ' + JSON.stringify(s)), 'keyword');
+
+export default class SearchUI {
+
+  constructor(client){
+    this.client = client;
+    this.searchResultsConf = null;
   }
 
 
   /**
    * Add a search bar
    */
-  this.searchResults = function(conf) {
+  searchBar(conf) {
+    /*const self = this;
+    const cb = function(results) {
+      self.resultsCallback(results, self)
+    }*/
+
+    const searchbar = new SearchBar();
+    searchbar.render(this.client, () => {}, conf);
+  }
+
+
+  /**
+   * Add a search bar
+   */
+  searchResults(conf) {
     this.searchResultsConf = conf;
+
+    const self = this;
+    observeStore(getStore(), state => { return state.keyup.results; },
+      (s) => {
+        console.log('Render results!');
+        console.log(JSON.stringify(s));
+        self.resultsCallback(s, self);
+      },
+      'results'
+
+    );
+
   }
 
 
@@ -35,11 +79,12 @@ var searchui = function(client) {
   /**
    * Callback function when the search returns
    */
-  this.resultsCallback = function(results, scope) {
+  resultsCallback(results, scope) {
     if (scope.searchResultsConf) {
+      console.log('rendering results');
       searchresults(results, scope.searchResultsConf);
     }
   }
 }
 
-module.exports = searchui;
+// module.exports = searchui;
