c5883e5e9833f54dda1c53b523f243905a0ff16c
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-22 21:59:45 +0300

    Add history parameter name

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,4 +1,4 @@
-import { setHistory } from '../util/history';
+import { setHistory, HISTORY_PARAMETERS } from '../util/history';
 
 export const SEARCH_FETCH_START = 'SEARCH_FETCH_START';
 export const SEARCH_RESULTS = 'SEARCH_RESULTS';
@@ -6,7 +6,7 @@ export const SEARCH_RESULTS = 'SEARCH_RESULTS';
 
 export function search(client, keyword) {
   return dispatch => {
-    setHistory(keyword);
+    setHistory(HISTORY_PARAMETERS.SEARCH, keyword);
     dispatch(searchFetchStart());
     client.search(keyword, (res) => dispatch(searchResults(keyword, res)));
   }
