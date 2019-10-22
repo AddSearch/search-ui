8e147ad9005e9b7bf6891312ab2f79d35a359dbe
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-22 16:56:47 +0300

    Basic functionality

--- src/components/filtergroup/filtergroup.js
+++ src/components/filtergroup/filtergroup.js
@@ -1,27 +1,68 @@
 import './filtergroup.scss';
 import handlebars from 'handlebars';
-
+import { setFilters } from '../../actions/filters';
+import { search } from '../../actions/search';
+import { getStore } from '../../store';
 
 const TEMPLATE = `
   <div class="addsearch-filtergroup">        
     <h3>{{title}}</h3>
+    <ul>
     {{#each options}}
-      <div class="filter">
-        <h3>{{label}}</h3>        
-      </div>
+      <li data-filter="{{filter}}">
+        {{label}}
+      </li>
     {{/each}}
+    </ul>
   </div>
 `;
 
 
 export default class FilterGroup {
 
-  constructor(filterGroupConf) {
-    this.filterGroupConf = filterGroupConf;
+  constructor(client, conf) {
+    this.client = client;
+    this.conf = conf;
   }
 
+
+  setFilters(container) {
+    const options = container.getElementsByTagName('li');
+    let filterString = '';
+    for (let i=0; i<options.length; i++) {
+      let o = options[i];
+      if (o.getAttribute('data-active') === 'true') {
+        filterString = o.getAttribute('data-filter');
+      }
+    }
+    console.log('Filter string ' + filterString);
+    getStore().dispatch(setFilters(this.client, filterString));
+    const keyword = getStore().getState().keyword.value;
+    if (keyword && keyword !== '') {
+      getStore().dispatch(search(this.client, keyword));
+    }
+  }
+
+
   render() {
-    const html = handlebars.compile(this.filterGroupConf.template || TEMPLATE)(this.filterGroupConf);
-    document.getElementById(this.filterGroupConf.containerId).innerHTML = html;
+    const html = handlebars.compile(this.conf.template || TEMPLATE)(this.conf);
+    const container = document.getElementById(this.conf.containerId);
+    container.innerHTML = html;
+
+    const options = container.getElementsByTagName('li');
+    const self = this;
+    for (let i=0; i<options.length; i++) {
+      let o = options[i];
+      o.onclick = (e) => {
+        if (o.getAttribute('data-active') === 'true') {
+          o.setAttribute('data-active', 'false');
+          self.setFilters(container);
+        }
+        else {
+          o.setAttribute('data-active', 'true');
+          self.setFilters(container);
+        }
+      };
+    }
   }
 }
