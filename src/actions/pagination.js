32f568903e5c1b5a477ae49d6a015bbe626eaac5
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-24 09:15:36 +0300

    Initial commit

---
+++ src/actions/pagination.js
@@ -0,0 +1,13 @@
+import { setHistory, HISTORY_PARAMETERS } from '../util/history';
+
+export const SET_PAGE = 'SET_PAGE';
+
+export function setPage(client, page) {
+  setHistory(HISTORY_PARAMETERS.PAGE, page);
+  client.setPaging(page, 10, 'relevance', 'desc');
+
+  return {
+    type: SET_PAGE,
+    page
+  }
+}
