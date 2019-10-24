4ac24e508673a968ae0a2c85ceb7e6408bc047a6
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-24 09:15:46 +0300

    Add pagination

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import filters from './filters';
 import keyword from './keyword';
+import pagination from './pagination';
 import search from './search';
 
 const reducer = combineReducers({
   filters,
   keyword,
+  pagination,
   search
 });
 
