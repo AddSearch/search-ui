e3f6f874f75979b7f7adb256e08907c236eaff43
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-24 09:16:17 +0300

    Remove client

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,6 +1,6 @@
 import { setHistory, HISTORY_PARAMETERS } from '../util/history';
 
-export const SET_FILTERS = 'ADD_FILTER';
+export const SET_FILTERS = 'SET_FILTERS';
 
 export function setFilters(client, filters) {
   setHistory(HISTORY_PARAMETERS.FILTERS, filters);
@@ -8,7 +8,6 @@ export function setFilters(client, filters) {
 
   return {
     type: SET_FILTERS,
-    filters,
-    client
+    filters
   }
 }
