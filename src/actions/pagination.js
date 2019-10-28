d45e8fd25760629bc0ea23a6ec4dfa09d3b2f514
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-28 16:02:31 +0200

    Set page as String to support History state on Chrome

--- src/actions/pagination.js
+++ src/actions/pagination.js
@@ -3,7 +3,7 @@ import { setHistory, HISTORY_PARAMETERS } from '../util/history';
 export const SET_PAGE = 'SET_PAGE';
 
 export function setPage(client, page) {
-  setHistory(HISTORY_PARAMETERS.PAGE, page);
+  setHistory(HISTORY_PARAMETERS.PAGE, page + '');
   client.setPaging(page, 10, 'relevance', 'desc');
 
   return {
