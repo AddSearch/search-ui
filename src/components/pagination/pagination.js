eee33a23b2766ee2ac78c3d4602be3a906d1610c
Antti Ala-Ilkka <antti@instereo.fi>
2019-10-31 14:39:20 +0200

    Add styling

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -9,15 +9,15 @@ import { getStore } from '../../store';
 const TEMPLATE = `
   <div class="addsearch-pagination">    
     {{#gt currentPage 1}}
-      <button data-page="previous">Previous</button>
+      <button data-page="previous" aria-label="Previous page">&#171;</button>
     {{/gt}}
     {{#each pages}}
-      <button data-page="{{this}}" {{#equals ../currentPage this}}data-active="true"{{/equals}}>
+      <button data-page="{{this}}" aria-label="Go to results page {{this}}" {{#equals ../currentPage this}}data-active="true"{{/equals}}>
         {{this}}
       </button>
     {{/each}}
     {{#lt currentPage lastPage}}
-      <button data-page="next">Next</button>
+      <button data-page="next" aria-label="Next page">&#187;</button>
     {{/lt}}
   </div>
 `;
