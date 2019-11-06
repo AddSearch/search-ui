a6a0102c6eb5af2e0a879f12092dcc33e0725d07
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-06 14:55:53 +0200

    Clear search results if keyword is removed

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,17 +1,16 @@
 /* global window */
 import { setHistory, HISTORY_PARAMETERS } from '../util/history';
 
-export const SEARCH_NO_KEYWORD = 'SEARCH_NO_KEYWORD';
 export const SEARCH_FETCH_START = 'SEARCH_FETCH_START';
 export const SEARCH_RESULTS = 'SEARCH_RESULTS';
 export const CLEAR_SEARCH_RESULTS = 'CLEAR_SEARCH_RESULTS';
 
 
 export function search(client, keyword, onResultsScrollTo) {
-  // Don't search if there is no keyword
+  // Clear search results if there is no keyword
   if (!keyword || keyword === '') {
     return {
-      type: SEARCH_NO_KEYWORD
+      type: CLEAR_SEARCH_RESULTS
     }
   }
   return dispatch => {
