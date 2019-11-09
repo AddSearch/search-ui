9e34fcb2ca763feeaac574a0fa8c0fdfae29eb5c
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-09 20:53:46 +0200

    Update value on new props

--- src/components/sortby/sortby.js
+++ src/components/sortby/sortby.js
@@ -1,6 +1,7 @@
 import './sortby.scss';
 import handlebars from 'handlebars';
-import { setPage } from '../../actions/pagination';
+
+import { sortBy } from '../../actions/sortby';
 import { search } from '../../actions/search';
 import { getStore } from '../../store';
 
@@ -8,7 +9,7 @@ const TEMPLATE = `
   <div class="addsearch-sortby">        
     <select>
       {{#each options}}
-        <option>{{label}}</option>
+        <option data-field={{sortBy}} data-order={{order}}>{{label}}</option>
       {{/each}}
     </select>
   </div>
@@ -23,9 +24,40 @@ export default class SortBy {
   }
 
 
-  render() {
+  onChange(select) {
+    const selectedOption = select.options[select.selectedIndex];
+    const field = selectedOption.getAttribute('data-field');
+    const order = selectedOption.getAttribute('data-order');
+
+    // Dispatch filter string
+    getStore().dispatch(sortBy(this.client, field, order));
+
+    // Refresh search
+    const keyword = getStore().getState().keyword.value;
+    getStore().dispatch(search(this.client, keyword));
+  }
+
+
+  render(props) {
     const html = handlebars.compile(this.conf.template || TEMPLATE)(this.conf);
     const container = document.getElementById(this.conf.containerId);
     container.innerHTML = html;
+
+    // Attach listeners
+    container.querySelector('select').onchange = (e) => { this.onChange(e.target) };
+
+    // Pre-selected option
+    if (props) {
+      const { field, order } = props;
+      const options = container.getElementsByTagName('option');
+
+      for (let i=0; i<options.length; i++) {
+        if (options[i].getAttribute('data-field') === field &&
+            options[i].getAttribute('data-order') === order) {
+          container.querySelector('select').value = options[i].text;
+          break;
+        }
+      }
+    }
   }
 }
