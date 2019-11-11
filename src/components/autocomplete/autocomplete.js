73b4b4f65be7bd2d14402175639270a2818eecf3
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-11 21:08:09 +0200

    Add search type autocomplete

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -1,18 +1,32 @@
 import './autocomplete.scss';
 import handlebars from 'handlebars';
 
-import { getSuggestions } from '../../actions/searchsuggestions';
+import { autocompleteSuggestions, autocompleteSearch } from '../../actions/autocomplete';
 import { getStore, observeStoreByKey } from '../../store';
 
 const TEMPLATE = `
   <div class="addsearch-autocomplete">
-    {{#gt items 0}}
+    {{#gt itemCount 0}}
       <div class="items">
-      {{#each ../suggestions}}
-        <div>
-         {{value}}
-        </div>
-      {{/each}}
+        {{#gt ../suggestions.length 0}}
+          <div class="suggestions">
+          {{#each ../../suggestions}}
+            <div>
+             {{value}}
+            </div>
+          {{/each}}
+          </div>
+        {{/gt}}
+        
+        {{#gt ../searchResults.length 0}}
+          <div class="searchresults">
+          {{#each ../../searchResults}}
+            <div>
+             {{title}}
+            </div>
+          {{/each}}
+          </div>
+        {{/gt}}
       </div>
     {{/gt}}
   </div>
@@ -25,29 +39,35 @@ export default class Autocomplete {
     this.client = client;
     this.conf = conf;
 
-    observeStoreByKey(getStore(), 'searchsuggestions', () => this.render());
+    observeStoreByKey(getStore(), 'autocomplete', () => this.render());
     observeStoreByKey(getStore(), 'keyword', (v) => this.keywordChanged(v));
   }
 
 
   keywordChanged(kw) {
-    // Fetch suggestions if keyword was typed. Not externally set (by browser's back button or like)
-    if (kw.externallySet === false) {
-      getStore().dispatch(getSuggestions(this.client, kw.value));
-    }
-    // Clear
-    else {
-      getStore().dispatch(getSuggestions(this.client, null));
-    }
+    // Fetch suggestions if keyword was typed, not externally set (by browser's back button)
+    const keyword = kw.externallySet === false ? kw.value : null;
+
+    this.conf.sources.forEach(v => {
+      if (v.type === 'suggestions') {
+        getStore().dispatch(autocompleteSuggestions(v.client, kw.value));
+      }
+      else if (v.type === 'search') {
+        getStore().dispatch(autocompleteSearch(v.client, kw.value));
+      }
+    });
   }
 
 
   render() {
-    const suggestions = getStore().getState().searchsuggestions.suggestions;
+    const suggestions = getStore().getState().autocomplete.suggestions;
+    const searchResults = getStore().getState().autocomplete.searchResults;
+    console.log(searchResults);
 
     const data = {
-      items: suggestions.length,
-      suggestions
+      itemCount: suggestions.length + searchResults.length,
+      suggestions,
+      searchResults
     };
 
     const html = handlebars.compile(this.conf.template || TEMPLATE)(data);
