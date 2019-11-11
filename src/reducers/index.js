a25a3254b7ebc5498f9ef13fe988b5a8e9576296
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-11 21:07:32 +0200

    Rename for broader usage. Add support for search-type autocomplete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
 import { combineReducers } from 'redux';
+import autocomplete from './autocomplete';
 import filters from './filters';
 import keyword from './keyword';
 import pagination from './pagination';
 import search from './search';
-import searchsuggestions from './searchsuggestions';
 import sortby from './sortby';
 
 const reducer = combineReducers({
+  autocomplete,
   filters,
   keyword,
   pagination,
   search,
-  searchsuggestions,
   sortby
 });
 
