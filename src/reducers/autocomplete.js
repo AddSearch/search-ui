458a564ab71840232d9dc9d33d897ad6630dcb3b
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-12 10:24:34 +0200

    Keep track on pending API requests

--- src/reducers/autocomplete.js
+++ src/reducers/autocomplete.js
@@ -1,35 +1,46 @@
 import {
+  AUTOCOMPLETE_FETCH,
   AUTOCOMPLETE_SUGGESTIONS_RESULTS,
-  CLEAR_AUTOCOMPLETE_SUGGESTIONS_RESULTS,
+  AUTOCOMPLETE_SUGGESTIONS_CLEAR,
   AUTOCOMPLETE_SEARCH_RESULTS,
-  CLEAR_AUTOCOMPLETE_SEARCH_RESULTS
+  AUTOCOMPLETE_SEARCH_CLEAR
 } from '../actions/autocomplete';
 
 const initialState = {
+  pendingRequests: 0,
   suggestions: [],
-  searchResults: []
+  searchResults: {}
 };
 
 export default function searchsuggestions(state = initialState, action) {
   switch (action.type) {
-    case CLEAR_AUTOCOMPLETE_SUGGESTIONS_RESULTS:
+    case AUTOCOMPLETE_FETCH:
+      return Object.assign({}, state, {
+        pendingRequests: state.pendingRequests + 1
+      });
+
+    case AUTOCOMPLETE_SUGGESTIONS_CLEAR:
       return Object.assign({}, state, {
         suggestions: []
       });
 
     case AUTOCOMPLETE_SUGGESTIONS_RESULTS:
       return Object.assign({}, state, {
+        pendingRequests: state.pendingRequests - 1,
         suggestions: action.results.suggestions
       });
 
-    case CLEAR_AUTOCOMPLETE_SEARCH_RESULTS:
+    case AUTOCOMPLETE_SEARCH_CLEAR:
       return Object.assign({}, state, {
-        searchResults: []
+        searchResults: {}
       });
 
     case AUTOCOMPLETE_SEARCH_RESULTS:
+      const nextSearchResults = Object.assign({}, state.searchResults);
+      nextSearchResults[action.jsonKey] = action.results.hits;
       return Object.assign({}, state, {
-        searchResults: action.results.hits
+        pendingRequests: state.pendingRequests - 1,
+        searchResults: nextSearchResults
       });
 
     default:
