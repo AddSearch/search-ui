961d78f34eb0bc627a71a86dda8fc8d353fda1fa
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-17 18:09:38 +0200

    Add support for facets

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,8 +1,14 @@
-import { TOGGLE_FILTER, REGISTER_FILTER, SET_ACTIVE_FILTERS} from '../actions/filters';
+import {
+  TOGGLE_FILTER,
+  REGISTER_FILTER,
+  SET_ACTIVE_FILTERS,
+  TOGGLE_FACET_FILTER
+} from '../actions/filters';
 
 const initialState = {
   availableFilters: [],
   activeFilters: {},
+  activeFacets: {},
   refreshSearch: true
 };
 
@@ -40,6 +46,29 @@ export default function filters(state = initialState, action) {
       return Object.assign({}, state, {
         activeFilters: action.json || {},
         refreshSearch: false
+      });
+
+
+    case TOGGLE_FACET_FILTER:
+      let nextActiveFacets = Object.assign({}, state.activeFacets);
+
+      if (!nextActiveFacets[action.field]) {
+        nextActiveFacets[action.field] = {};
+      }
+
+      // Remove filter
+      if (nextActiveFacets[action.field][action.value]) {
+        delete nextActiveFacets[action.field][action.value];
+      }
+      // Add filter
+      else {
+        nextActiveFacets[action.field][action.value] = 'true';
+      }
+      nextActiveFacets['v'] = !nextActiveFacets['v'] ? 1 : nextActiveFacets['v']+1;
+
+      return Object.assign({}, state, {
+        activeFacets: nextActiveFacets,
+        refreshSearch: true
       });
 
 
