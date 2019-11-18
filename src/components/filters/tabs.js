be20cf6797781dcc0cb04a2353a547f79e200a8e
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-18 15:36:22 +0200

    Add Tabs filters

--- src/components/filters/tabs.js
+++ src/components/filters/tabs.js
@@ -1,18 +1,9 @@
-import './filters.scss';
-import handlebars from 'handlebars';
-
-const TEMPLATE = `
+export const TABS_TEMPLATE = `
   <div class="addsearch-filters-tabs">
     <div class="tabs">
       {{#each options}}
-        <button {{#if active}}class="active"{{/if}}>{{label}}</button>
+        <button data-filter="{{@key}}" {{#if active}}class="active"{{/if}}>{{label}}</button>
       {{/each}}
     </div>
   </div>
 `;
-
-export function tabs(conf) {
-  const html = handlebars.compile(conf.template || TEMPLATE)(conf);
-  const container = document.getElementById(conf.containerId);
-  container.innerHTML = html;
-}
