0b0048a04fc78b2a4787f68efa8330867bc62614
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-19 10:10:58 +0200

    Add actions to change active suggestion

--- src/reducers/autocomplete.js
+++ src/reducers/autocomplete.js
@@ -4,12 +4,15 @@ import {
   AUTOCOMPLETE_SUGGESTIONS_CLEAR,
   AUTOCOMPLETE_SEARCH_RESULTS,
   AUTOCOMPLETE_SEARCH_CLEAR,
-  AUTOCOMPLETE_HIDE
+  AUTOCOMPLETE_HIDE,
+  KEYBOARD_EVENT, ARROW_UP, ARROW_DOWN,
+  SET_ACTIVE_SUGGESTION
 } from '../actions/autocomplete';
 
 const initialState = {
   pendingRequests: 0,
   suggestions: [],
+  activeSuggestionIndex: null,
   searchResults: {},
   hide: false
 };
@@ -23,13 +26,15 @@ export default function searchsuggestions(state = initialState, action) {
 
     case AUTOCOMPLETE_SUGGESTIONS_CLEAR:
       return Object.assign({}, state, {
-        suggestions: []
+        suggestions: [],
+        activeSuggestionIndex: null
       });
 
     case AUTOCOMPLETE_SUGGESTIONS_RESULTS:
       return Object.assign({}, state, {
         pendingRequests: state.pendingRequests - 1,
         suggestions: action.results.suggestions,
+        activeSuggestionIndex: null,
         hide: false
       });
 
@@ -49,7 +54,43 @@ export default function searchsuggestions(state = initialState, action) {
 
     case AUTOCOMPLETE_HIDE:
       return Object.assign({}, state, {
-        hide: true
+        hide: true,
+        activeSuggestionIndex: null
+      });
+
+    case SET_ACTIVE_SUGGESTION:
+      return Object.assign({}, state, {
+        activeSuggestionIndex: action.index
+      });
+
+    case KEYBOARD_EVENT:
+      let nextActiveSuggestion = state.activeSuggestionIndex;
+      if (action.direction === ARROW_DOWN) {
+        if (nextActiveSuggestion === null && state.suggestions.length > 0) {
+          nextActiveSuggestion = 0;
+        }
+        else if (nextActiveSuggestion === state.suggestions.length-1) {
+          nextActiveSuggestion = null;
+        }
+        else {
+          nextActiveSuggestion = nextActiveSuggestion + 1;
+        }
+      }
+      else if (action.direction === ARROW_UP) {
+        if (nextActiveSuggestion === null && state.suggestions.length > 0) {
+          nextActiveSuggestion = state.suggestions.length-1;
+        }
+        else if (nextActiveSuggestion === 0) {
+          nextActiveSuggestion = null;
+        }
+        else {
+          nextActiveSuggestion = nextActiveSuggestion - 1;
+        }
+      }
+console.log('Setting activeSuggestionIndex from ' + state.activeSuggestionIndex + ' to ' + nextActiveSuggestion);
+      return Object.assign({}, state, {
+        hide: false,
+        activeSuggestionIndex: nextActiveSuggestion
       });
 
     default:
