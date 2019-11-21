bbeb79c0a46a52efee6782a4710fabcc0964c73b
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-21 15:55:09 +0200

    Make "Number of results" template customizable

--- src/components/searchresults/searchresults.js
+++ src/components/searchresults/searchresults.js
@@ -7,12 +7,14 @@ import { renderToContainer } from '../../util/dom';
 const TEMPLATE = `
   <div class="addsearch-searchresults">    
     {{#if resultcount}}
-      <p>{{total_hits}} search results</p>
+      {{> numberOfResultsTemplate }}
     {{/if}}
     
     {{#each hits}}
       <div class="hit">
-        <h3>{{title}}</h3>
+        <h3>
+          <a href="{{url}}">{{title}}</a>
+        </h3>
         <p>
           <span class="main-image" style="background-image: url(data:image/jpeg;base64,{{images.main_b64}})">
             <img src="{{images.main}}" alt="{{title}}" />
@@ -24,11 +26,14 @@ const TEMPLATE = `
   </div>
 `;
 
-
 const TEMPLATE_NO_RESULTS = `
   <div class="addsearch-searchresults addsearch-searchresults-no-results">    
     <h2>No search results with keyword <em>{{keyword}}</em></h2>
   </div>
+`;
+
+const TEMPLATE_NUMBER_OF_RESULTS = `
+  <div class="number-of-results">{{total_hits}} search results</div>
 `;
 
 
@@ -37,6 +42,8 @@ export default class SearchResults {
   constructor(conf) {
     this.conf = conf;
     observeStoreByKey(getStore(), 'search', () => this.render());
+
+    handlebars.registerPartial('numberOfResultsTemplate', this.conf.template_resultcount || TEMPLATE_NUMBER_OF_RESULTS);
   }
 
 
@@ -48,7 +55,7 @@ export default class SearchResults {
 
     let template = this.conf.template || TEMPLATE;
     if (data.hits && data.hits.length === 0) {
-      template = this.conf.template_no_results || TEMPLATE_NO_RESULTS;
+      template = this.conf.template_noresults || TEMPLATE_NO_RESULTS;
     }
 
     renderToContainer(this.conf.containerId, template, data);
