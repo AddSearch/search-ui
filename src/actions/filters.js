2a370b49b5aa5f01fdfab70470da47690fed4f90
Antti Ala-Ilkka <antti@instereo.fi>
2019-11-28 21:31:20 +0200

    Add action to clear selected filters and facets

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -3,6 +3,7 @@ export const REGISTER_FILTER = 'REGISTER_FILTER';
 export const SET_ACTIVE_FILTERS = 'SET_ACTIVE_FILTERS';
 export const SET_ACTIVE_FACETS = 'SET_ACTIVE_FACETS';
 export const TOGGLE_FACET_FILTER = 'TOGGLE_FACET_FILTER';
+export const CLEAR_SELECTED_FILTERS_AND_FACETS = 'CLEAR_SELECTED_FILTERS_AND_FACETS';
 
 export function registerFilter(filterObj) {
   return {
@@ -33,10 +34,17 @@ export function setActiveFacets(json) {
     json
   }
 }
+
 export function toggleFacetFilter(field, value) {
   return {
     type: TOGGLE_FACET_FILTER,
     field,
     value
+  }
+}
+
+export function clearSelected() {
+  return {
+    type: CLEAR_SELECTED_FILTERS_AND_FACETS
   }
 }
