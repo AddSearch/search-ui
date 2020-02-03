51e620a87800ed39a7ac188b18943013851736b0
Antti Ala-Ilkka <antti@instereo.fi>
2020-02-03 14:52:32 +0200

    Support parent and grandparent elements for docid data attribute

--- src/util/analytics.js
+++ src/util/analytics.js
@@ -1,14 +1,14 @@
 /**
  * Add click trackers to search result links
  */
-export function addClickTrackers(client, linkArray, searchResultArray) {
+export function addClickTrackers(client, linkArray, searchResults) {
   if (linkArray && linkArray.length > 0) {
     for (let i=0; i<linkArray.length; i++) {
 
       // Enter pressed when link is active
       linkArray[i].addEventListener('keyup', (e) => {
         if (e.keyCode === 13) {
-          onLinkClick(e, client, searchResultArray);
+          onLinkClick(e, client, searchResults);
         }
       });
 
@@ -18,7 +18,7 @@ export function addClickTrackers(client, linkArray, searchResultArray) {
         if (e.button && e.buttons && e.button === e.buttons) {
           return;
         }
-        onLinkClick(e, client, searchResultArray);
+        onLinkClick(e, client, searchResults);
       });
 
     }
@@ -26,22 +26,26 @@ export function addClickTrackers(client, linkArray, searchResultArray) {
 }
 
 
-function onLinkClick(e, client, searchResultData) {
-  const docid = e.target.getAttribute('data-analytics-click');
-  const position = getDocumentPosition(client.getSettings().paging.pageSize, searchResultData, docid);
+function onLinkClick(e, client, searchResults) {
+  // Support data attributes in parent and grandparent elements
+  const docid = e.target.getAttribute('data-analytics-click') ||
+    e.target.parentNode.getAttribute('data-analytics-click') ||
+    e.target.parentNode.parentNode.getAttribute('data-analytics-click');
+  const position = getDocumentPosition(client.getSettings().paging.pageSize, searchResults, docid);
   client.searchResultClicked(docid, position);
 }
 
 
-export function getDocumentPosition(pageSize, results, docid) {
-  if (results && results.hits) {
+export function getDocumentPosition(pageSize, searchResults, docid) {
+
+  if (searchResults && searchResults.hits) {
     // Calculate offset if the user is not on results page 1
-    const currentPage = results.page || 1;
+    const currentPage = searchResults.page || 1;
     const offset = (currentPage - 1) * pageSize;
 
     // Find document's position in results array
-    for (let i=0; i<results.hits.length; i++) {
-      if (results.hits[i].id === docid) {
+    for (let i=0; i<searchResults.hits.length; i++) {
+      if (searchResults.hits[i].id === docid) {
         return offset + (i+1);
       }
     }
