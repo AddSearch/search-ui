dc444a8fa66af2d68b03fe15978fd38f9bbf79d0
Antti Ala-Ilkka <antti@instereo.fi>
2020-02-03 13:54:28 +0200

    Initial commit

---
+++ src/components/segmentedresults/segmentedresults.js
@@ -0,0 +1,20 @@
+import { getStore, observeStoreByKey } from '../../store';
+import { renderToContainer, validateContainer } from '../../util/dom';
+
+
+export default class SegmentedResults {
+
+  constructor(client, conf) {
+    this.client = client;
+    this.conf = conf;
+
+    if (validateContainer(conf.containerId)) {
+      observeStoreByKey(getStore(), 'segmentedsearch', (data) => this.render(data));
+    }
+  }
+
+
+  render(data) {
+    renderToContainer(this.conf.containerId, this.conf.template, data);
+  }
+}
