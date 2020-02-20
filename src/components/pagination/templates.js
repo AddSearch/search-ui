ff91fc429a2950cd6c4922c6c5a5beb8a79656bc
sami-petrisalonen <sami@addsearch.com>
2020-02-20 08:46:39 +0200

    Overall style fixes to default template

--- src/components/pagination/templates.js
+++ src/components/pagination/templates.js
@@ -1,7 +1,7 @@
 export const PAGINATION_TEMPLATE = `
   <div class="addsearch-pagination">    
     {{#gt currentPage 1}}
-      <button data-page="previous" aria-label="Previous page">&#171;</button>
+      <button data-page="previous" aria-label="Previous page">&#x25c0;</button>
     {{/gt}}
     {{#each pages}}
       <button data-page="{{this}}" aria-label="Go to results page {{this}}" {{#equals ../currentPage this}}data-active="true" aria-current="true"{{/equals}}>
@@ -9,7 +9,7 @@ export const PAGINATION_TEMPLATE = `
       </button>
     {{/each}}
     {{#lt currentPage lastPage}}
-      <button data-page="next" aria-label="Next page">&#187;</button>
+      <button data-page="next" aria-label="Next page">&#x25ba;</button>
     {{/lt}}
   </div>
 `;
