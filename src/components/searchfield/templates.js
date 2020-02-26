b8f0758cc8f81b674a0c90bac2cb5d16ddbd76b1
Antti Ala-Ilkka <antti@instereo.fi>
2020-02-26 13:49:26 +0200

    Remove extra element

--- src/components/searchfield/templates.js
+++ src/components/searchfield/templates.js
@@ -1,7 +1,5 @@
 export const SEARCHFIELD_TEMPLATE = `
   <form class="addsearch-searchfield" autocomplete="off" action="?" role="search">
-    <!--<span class="search-icon">&#x2315;</span>-->
-    <?xml version="1.0" encoding="UTF-8"?>
     <div class="search-field-wrapper">     
       <input type="search" placeholder="{{placeholder}}" aria-label="Search field" class="{{#not icon false}}icon{{/not}}" />
     </div>
