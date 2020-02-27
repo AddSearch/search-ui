45eadc8f68499ed0821fb2fe26fda949d60cd0e5
sami-petrisalonen <sami@addsearch.com>
2020-02-27 15:13:32 +0200

    searchfield container + style fixes

--- src/components/searchfield/templates.js
+++ src/components/searchfield/templates.js
@@ -1,4 +1,5 @@
 export const SEARCHFIELD_TEMPLATE = `
+<div class="addsearch-searchfield-container">
   <form class="addsearch-searchfield" autocomplete="off" action="?" role="search">
     <div class="search-field-wrapper">     
       <input type="search" placeholder="{{placeholder}}" aria-label="Search field" class="{{#not icon false}}icon{{/not}}" />
@@ -7,4 +8,5 @@ export const SEARCHFIELD_TEMPLATE = `
       <button type="button" aria-label="Search button" >{{button}}</button>
     {{/if}}
   </form>
+</div>
 `;
