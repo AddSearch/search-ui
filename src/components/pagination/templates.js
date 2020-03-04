4b79fac6ef61834ea2b469a20a3f5690f758d098
sami-petrisalonen <sami@addsearch.com>
2020-03-04 15:53:18 +0200

    used filters mobile style fix, pagination unicode icon fix

--- src/components/pagination/templates.js
+++ src/components/pagination/templates.js
@@ -1,7 +1,7 @@
 export const PAGINATION_TEMPLATE = `
   <div class="addsearch-pagination">    
     {{#gt currentPage 1}}
-      <button data-page="previous" aria-label="Previous page">&#x25c0;</button>
+      <button data-page="previous" aria-label="Previous page">❮</button>
     {{/gt}}
     {{#each pages}}
       <button data-page="{{this}}" aria-label="Go to results page {{this}}" {{#equals ../currentPage this}}data-active="true" aria-current="true"{{/equals}}>
@@ -9,7 +9,7 @@ export const PAGINATION_TEMPLATE = `
       </button>
     {{/each}}
     {{#lt currentPage lastPage}}
-      <button data-page="next" aria-label="Next page">&#x25ba;</button>
+      <button data-page="next" aria-label="Next page">❯</button>
     {{/lt}}
   </div>
 `;
