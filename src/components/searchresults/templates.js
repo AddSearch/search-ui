9d63f1016d73b163ab229590e6664d80097bc64f
Antti Ala-Ilkka <antti@instereo.fi>
2020-03-10 10:13:44 +0200

    Show promoted results' images

--- src/components/searchresults/templates.js
+++ src/components/searchresults/templates.js
@@ -5,17 +5,20 @@ export const SEARCHRESULTS_TEMPLATE = `
     {{/if}}
     
     {{#each hits}}
-      <div class="hit">
+      <div class="hit{{#equals type "PROMOTED"}} promoted{{/equals}}">
         <h3>
           <a href="{{url}}" data-analytics-click="{{id}}">{{title}}</a>
         </h3>
         <div class="highlight">
           {{> searchResultImageTemplate}}
           {{{highlight}}}..
+          {{categories.length}}
         </div>
-        <div class="category">
-          {{selectCategory this}}
-        </div>
+        {{#gt categories.length 1}}
+          <div class="category">
+            {{selectCategory ..}}
+          </div>
+        {{/gt}}
       </div>
     {{/each}}
   </div>
@@ -24,7 +27,8 @@ export const SEARCHRESULTS_TEMPLATE = `
 
 export const SEARCHRESULT_IMAGE_TEMPLATE = `
   <span class="main-image {{document_type}} {{#unless images.main}}noimage{{/unless}}"{{#if images.main}} style="background-image: url(data:image/jpeg;base64,{{images.main_b64}})"{{/if}}>
-    {{#if images.main}}<img src="{{images.main}}" alt="{{title}}" />{{else}}{{/if}}
+    {{#if images.main}}<img src="{{images.main}}" alt="{{title}}" />
+    {{else if style.image_url}}<img src="{{style.image_url}}" alt="{{title}}" />{{/if}}    
   </span>
 `;
 
