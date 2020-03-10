b86df817cdd65ccee3b2bc7837c3d8e65638b1b0
Antti Ala-Ilkka <antti@instereo.fi>
2020-03-10 10:00:14 +0200

    Move search result image to a separate template

--- src/components/searchresults/templates.js
+++ src/components/searchresults/templates.js
@@ -10,9 +10,7 @@ export const SEARCHRESULTS_TEMPLATE = `
           <a href="{{url}}" data-analytics-click="{{id}}">{{title}}</a>
         </h3>
         <div class="highlight">
-          <span class="main-image {{document_type}} {{#unless images.main}}noimage{{/unless}}"{{#if images.main}} style="background-image: url(data:image/jpeg;base64,{{images.main_b64}})"{{/if}}>
-            {{#if images.main}}<img src="{{images.main}}" alt="{{title}}" />{{else}}{{/if}}
-          </span>
+          {{> searchResultImageTemplate}}
           {{{highlight}}}..
         </div>
         <div class="category">
@@ -24,10 +22,17 @@ export const SEARCHRESULTS_TEMPLATE = `
 `;
 
 
+export const SEARCHRESULT_IMAGE_TEMPLATE = `
+  <span class="main-image {{document_type}} {{#unless images.main}}noimage{{/unless}}"{{#if images.main}} style="background-image: url(data:image/jpeg;base64,{{images.main_b64}})"{{/if}}>
+    {{#if images.main}}<img src="{{images.main}}" alt="{{title}}" />{{else}}{{/if}}
+  </span>
+`;
+
+
 export const NUMBER_OF_RESULTS_TEMPLATE = `
   <div class="number-of-results">
     {{#gt page 1}}Page {{../page}} of {{/gt}}
-    {{total_hits}} search results
+    {{total_hits}}{{#equals total_hits 10000}}+{{/equals}} results
   </div>
 `;
 
