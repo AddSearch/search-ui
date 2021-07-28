d545d7d9ff88ef751c4edade74c77f987611377c
Hao Phan <haophan@Haos-MacBook-Pro.local>
2021-07-28 16:15:45 +0300

    add 'fieldForJumpSearch' to the searchField setting

--- src/components/searchfield/searchfield.js
+++ src/components/searchfield/searchfield.js
@@ -64,7 +64,7 @@ export default class SearchField {
     if (kw.indexOf(WARMUP_QUERY_PREFIX) !== 0) {
       this.reduxStore.dispatch(setPage(client, 1, null, this.reduxStore));
     }
-    this.onSearch(kw, false, searchAsYouType);
+    this.onSearch(kw, false, searchAsYouType, this.conf.fieldForJumpSearch);
   }
 
 
--- src/index.js
+++ src/index.js
@@ -79,8 +79,9 @@ export default class AddSearchUI {
   }
 
 
-  executeSearch(keyword, onResultsScrollTo, searchAsYouType) {
-    this.reduxStore.dispatch(search(this.client, keyword, onResultsScrollTo, false, searchAsYouType, this.reduxStore));
+  executeSearch(keyword, onResultsScrollTo, searchAsYouType, fieldForJumpSearch) {
+    this.reduxStore.dispatch(search(this.client, keyword, onResultsScrollTo, false, searchAsYouType,
+      this.reduxStore, fieldForJumpSearch));
 
     for (let key in this.segmentedSearchClients) {
       this.reduxStore.dispatch(segmentedSearch(this.segmentedSearchClients[key], key, keyword));
@@ -117,8 +118,8 @@ export default class AddSearchUI {
    */
 
   searchField(conf) {
-    const onSearch = (keyword, onResultsScrollTo, searchAsYouType) =>
-      this.executeSearch(keyword, onResultsScrollTo, searchAsYouType);
+    const onSearch = (keyword, onResultsScrollTo, searchAsYouType, fieldForJumpSearch) =>
+      this.executeSearch(keyword, onResultsScrollTo, searchAsYouType, fieldForJumpSearch);
     new SearchField(this.client, this.reduxStore, conf, this.settings.matchAllQuery === true, onSearch);
   }
 
