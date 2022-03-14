c76d3b45e42a3e883566c5887be177869718abd6
Hao Phan <hao@addsearch.com>
2022-03-14 09:23:09 +0200

    fix expansion arrow

--- src/components/hierarchicalfacets/templates.js
+++ src/components/hierarchicalfacets/templates.js
@@ -7,18 +7,20 @@ export const FACETS_TEMPLATE = `
 `;
 
 export const SUB_HIERARCHICAL_FACETS_TEMPLATE = `
-  <li data-facet="{{value}}" {{#if (validateOpenState value)}}class="shrink"{{/if}}>
-    <label>
-      <input type="checkbox" value="{{value}}" data-field="{{field}}" />
-      <span>{{displayValue}}</span> <em>({{count}})</em>
-    </label>
-    {{#if children}}
-      <span class="addsearch-facet-group-expansion-arrow"></span>
-      <ul class="addsearch-facet-child-container">
-        {{#each children}}
-          {{> subHierarchicalFacetsTemplate this }}
-        {{/each}}
-      </ul>
-    {{/if}}
+  <li>
+    <div class="adds-facet-wrap {{#if (validateOpenState value)}}shrink{{/if}}" data-facet="{{value}}">
+      <span class="addsearch-facet-group-expansion-arrow {{#unless children}}adds-visibility-hidden{{/unless}}"></span>
+      <label>
+        <input type="checkbox" value="{{value}}" data-field="{{field}}" />
+        <span>{{displayValue}}</span> <em>({{count}})</em>
+      </label>
+      {{#if children}}
+        <ul class="addsearch-facet-child-container">
+          {{#each children}}
+            {{> subHierarchicalFacetsTemplate this }}
+          {{/each}}
+        </ul>
+      {{/if}}
+    </div>
   </li>
 `;
