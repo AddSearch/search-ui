ca278d18ed4df789f66b6eb2e499e39364dd2ddd
Hao Phan <hao@addsearch.com>
2022-06-13 09:12:02 +0300

    fix styles

--- src/components/recommendations/templates.js
+++ src/components/recommendations/templates.js
@@ -3,9 +3,9 @@ export const RECO_FBT_TEMPLATE = `
     {{#each hits}}
      <div class="hit">
         <div class="hit-top">         
-          <span class="main-image" style="background-image: url(data:image/jpeg;base64,{{custom_fields.image_link_b64}})">
+          <span class="main-image" style="background-image: url(data:image/jpeg;base64,{{images.main_b64}})">
             <a href="{{url}}">
-              <img src="https://d20vwa69zln1wj.cloudfront.net/2c32bf3eb06b30af5f8208481aea3e8b/main/fce0634d20f8a0446e7a7314d2900786-20120101.jpg" alt="{{title}}" />
+              <img src="{{images.main}}" alt="{{title}}" />
             </a>            
           </span>     
           <h3>
