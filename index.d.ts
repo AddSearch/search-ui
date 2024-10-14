a448640f4cb5c8f473780674ec5a480d48d9f37f
janstoff <jan.steinhoff@gmail.com>
2024-10-14 09:36:18 +0100

    add types for npm import with types

---
+++ index.d.ts
@@ -0,0 +1,69 @@
+declare module "addsearch-search-ui" {
+  export default class AddSearchUI {
+    constructor(client: any, settings?: any);
+    start(): void;
+    executeSearch(
+      keyword: string,
+      options?: any,
+      appendResults?: boolean,
+      fieldForInstantRedirect?: string,
+      fieldForInstantRedirectInConfiguration?: string
+    ): void;
+    fetchRecommendation(containerId: string): void;
+    searchField(conf: any): void;
+    autocomplete(conf: any): void;
+    searchResults(conf: any): void;
+    segmentedSearchResults(conf: any): void;
+    facets(conf: any): void;
+    hierarchicalFacets(conf: any): void;
+    rangeFacets(conf: any): void;
+    filters(conf: any): void;
+    sortBy(conf: any): void;
+    pagination(conf: any): void;
+    loadMore(conf: any): void;
+    activeFilters(conf: any): void;
+    recommendations(conf: any): void;
+    search(keyword: string): void;
+    hideAutocomplete(): void;
+    clear(): void;
+    registerHandlebarsHelper(name: string, helper: Function): void;
+    registerHandlebarsPartial(name: string, partial: string): void;
+  }
+
+  export const AUTOCOMPLETE_TYPE: {
+    SEARCH: string;
+    SUGGESTIONS: string;
+    CUSTOM_FIELDS: string;
+  };
+
+  export const FILTER_TYPE: {
+    CHECKBOX_GROUP: string;
+    RADIO_GROUP: string;
+    SELECT_LIST: string;
+    RANGE: string;
+    TABS: string;
+    TAGS: string;
+  };
+
+  export const SORTBY_TYPE: {
+    SELECT_LIST: string;
+    RADIO_GROUP: string;
+  };
+
+  export const LOAD_MORE_TYPE: {
+    BUTTON: string;
+    INFINITE_SCROLL: string;
+  };
+
+  export const RECOMMENDATION_TYPE: {
+    FREQUENTLY_BOUGHT_TOGETHER: string;
+    RELATED_ITEMS: string;
+  };
+
+  export const RANGE_FACETS_TYPE: {
+    CHECKBOX: string;
+    SLIDER: string;
+  };
+
+  export const Handlebars_runtime: any;
+}
