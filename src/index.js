ab40bd93c0a6dd0987ea056cb2024612b5cfe111
Hao Phan <hao@addsearch.com>
2025-10-06 15:43:40 +0300

    [sc-12706] fix SonarCloud reported issues

--- src/index.js
+++ src/index.js
@@ -163,9 +163,9 @@ export default class AddSearchUI {
   }
 
   fetchRecommendation(containerId) {
-    const recoSetting = this.recommendationsSettings.filter(
+    const recoSetting = this.recommendationsSettings.find(
       (setting) => setting.containerId === containerId
-    )[0];
+    );
     if (!recoSetting) return;
     this.reduxStore.dispatch(
       recommend(this.client, {
@@ -173,9 +173,9 @@ export default class AddSearchUI {
         type: recoSetting.type,
         blockId: recoSetting.blockId,
         configurationKey: recoSetting.configurationKey,
-        itemId: !recoSetting.getProductIdFunction
-          ? null
-          : recoSetting.getProductIdFunction.call(undefined, undefined)
+        itemId: recoSetting.getProductIdFunction
+          ? recoSetting.getProductIdFunction.call(undefined, undefined)
+          : null
       })
     );
   }
@@ -186,10 +186,10 @@ export default class AddSearchUI {
 
   initFromClientSettings() {
     const paging = this.client.getSettings().paging;
-    const hasPageParameterInUrl = new URLSearchParams(window.location.search).has(
+    const hasPageParameterInUrl = new URLSearchParams(globalThis.location.search).has(
       HISTORY_PARAMETERS.PAGE
     );
-    const hasSortByParameterInUrl = new URLSearchParams(window.location.search).has(
+    const hasSortByParameterInUrl = new URLSearchParams(globalThis.location.search).has(
       HISTORY_PARAMETERS.SORTBY
     );
     const shouldUpdateSortParameterOnPageLoad =
